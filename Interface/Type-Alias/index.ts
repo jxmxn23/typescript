b1f066b0f7bd8f99faab42a170789d9717804292
jimin <sjm043000@naver.com>
2024-08-12 21:50:58 +0900

    Add files via upload

---
+++ Interface/Type-Alias/index.ts
@@ -0,0 +1,17 @@
+type coupangItem = {
+  link: string;
+  name: string;
+  star: number;
+  review: number;
+  price: number;
+  discount: number;
+};
+
+const birakZero: coupangItem = {
+  link: "https://contents.kyobobook.co.kr/sih/fit-in/600x0/gift/pdt/1193/S1713245587978.jpg",
+  name: "팔도 비락식혜 제로",
+  star: 5,
+  review: 2235,
+  price: 26500,
+  discount: 52,
+};
---
+++ Interface/TypeLiteral/index.ts
@@ -0,0 +1,8 @@
+type Movie = {
+  name: string;
+  grade: "12" | "15" | "18" | "all";
+};
+const pliot: Movie = {
+  name: "파일럿",
+  grade: "12",
+};
